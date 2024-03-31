2c5407c974334bc1ed1a123e674a506ce7bc47c7
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-03-30 20:13:02 -0400

    Add project files.

---
+++ RazorCrudUI/Pages/Items/Create.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Domain.Models;
+using Domain.Interfaces;
+
+namespace UI.Pages.Items
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IItemRepository _repo;
+
+        public CreateModel(IItemRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        // we use bind property when we want to update things
+        [BindProperty]
+        public ItemModel ItemModel { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            await _repo.InsertItemAsync(ItemModel);
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorCrudUI/Pages/Items/Edit.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace UI.Pages.Items
+{
+    public class EditModel : PageModel
+    {
+        private readonly IItemRepository _repo;
+
+        public EditModel(IItemRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [BindProperty]
+        public ItemModel ItemModel { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemmodel = await _repo.GetItemByIDAsync(id.Value);
+            if (itemmodel == null)
+            {
+                return NotFound();
+            }
+            ItemModel = itemmodel;
+            return Page(); //has references to the properties of the ItemModel
+            // this is the same as return View(ItemModel);
+            // View is a method that returns a view result
+            // 
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var dbItem = await _repo.UpdateItemAsync(ItemModel);
+            if (dbItem == false)
+            {
+                ModelState.AddModelError(string.Empty, "Failed to update item.");
+                return Page();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
